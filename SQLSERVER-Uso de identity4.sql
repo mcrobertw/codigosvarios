31dec56934569e7db7dc70fe1fe7b889fb8f5795
Robert Moreira <robertmoreiracenteno@hotmail.com>
2020-02-21 00:52:39 -0500

    Create SQLSERVER-Uso de identity4.sql

---
+++ SQLSERVER-Uso de identity4.sql
@@ -0,0 +1,19 @@
+USE FARMACIA;
+
+DROP TABLE VENDEDOR;
+
+create table VENDEDOR(
+	idvendedor integer identity PRIMARY KEY,
+	nombreVen varchar(40),
+	direVen varchar(50),
+);
+
+insert into vendedor values('Juan Piguave', 'Portoviejo'),('Lorenzo Piguave','Manta');
+
+Delete from vendedor;
+
+dbcc CHECKIDENT('vendedor',RESEED,0);
+
+insert into vendedor values('luis Piguave', 'Portoviejo'),('Oliver Piguave','Manta');
+
+select *from vendedor;
