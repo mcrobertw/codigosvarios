6d558e3f1c6671f36047e729998eaa7ee1a39d39
Robert Moreira <robertmoreiracenteno@hotmail.com>
2020-12-30 15:59:07 -0500

    Update ajax_instanciación_XMLHTTPRequest.js
    corrección a doble new que se usaba en la instanciación

--- snippet/ajax_instanciación_XMLHTTPRequest.js
+++ snippet/ajax_instanciación_XMLHTTPRequest.js
@@ -1,7 +1,7 @@
 <script language="JavaScript" type="text/javascript">
 function getXMLHTTPRequest(){
 	try{
-		req=new new XMLHttpRequest(); /*p.e. firefox*/
+		req=new XMLHttpRequest(); /*p.e. firefox*/
 	}catch(err1){
 		try{
 			req=new ActiveXObject("Msxml2.XMLHTTP");/*Algunas versiones de I.E.*/
