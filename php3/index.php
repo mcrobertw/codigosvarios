194af7d58a0b838e5aae2075f2bfb9377b63716c
Robert Moreira <robertmoreiracenteno@hotmail.com>
2021-01-12 06:14:16 -0500

    uso de get en php

---
+++ php3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+	"https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="es" xmlns="https://www.w3.org/1999/xhtml/">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Ejemplo Formulario</title>
+</head>
+<body>
+	<form action="procesarorden.php" method="get">
+		Escribe tu nombre:
+		<input type="text" size="50" name="nombre"><br/>
+		<input type="submit" value="enviar"/>
+	</form>
+</body>
+</html>
