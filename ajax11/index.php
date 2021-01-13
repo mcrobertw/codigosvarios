9026d1f66eea0724df6c12e0f3a79274d111463e
Robert Moreira <robertmoreiracenteno@hotmail.com>
2021-01-13 05:10:44 -0500

    responsetext con html

---
+++ ajax11/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mi Aplicación Ajax</title>
+	<script language="JavaScript" type="text/javascript" src="ajax.js"></script>
+
+</head>
+<body onLoad="solicitudAjaxCliente()">
+	<b>El servidor dice: </b><br>
+	<div id="MiElementoPagina"></div>
+</body>
+</html>
---
+++ ajax11/textoservidor.php
@@ -0,0 +1,5 @@
+<?php 
+	echo "<h3>Devolver Texto Formateado</h3>";
+	echo "<hr/>";
+	echo utf8_encode("&#161Podemos usar HTML para <strong>Formatear</strong> el texto antes de devolverlo!");
+?>
