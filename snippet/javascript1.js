d67ba23541463c00750824ac8437d1453e5a1084
Robert Moreira <robertmoreiracenteno@hotmail.com>
2022-03-13 11:28:18 -0500

    actualizando detalles

--- snippet/javascript1.js
+++ snippet/javascript1.js
@@ -1,3 +1,5 @@
+
+/*Después de etiqueta "title" de html*/
 <script language="JavaScript" type="text/javascript">
 	function utilizarFuncionGlobal(){
 		var nuevosdatos = $('caja1','caja2','caja3');
@@ -6,3 +8,12 @@
 		$(nuevosdatos[2]).innerHTML="prototype.js";
 	}
 </script>
+
+/* En etiqueta body */
+
+<form>
+	<input type="button" onclick="utilizarFuncionGlobal()" value="Usando Prototype.js">
+</form>
+<div id="caja1" class="displaybox">a</div>
+<div id="caja2" class="displaybox">b</div>
+<div id="caja3" class="displaybox">c</div>
