360804b81383638893dcc7d3d2635fe731c8053d
Robert Moreira <robertmoreiracenteno@hotmail.com>
2022-03-12 23:09:16 -0500

    usando libreria para sustituir al método dom documentgetelementbyid

---
+++ ajax10/ajax_v2.js
@@ -0,0 +1,41 @@
+function getXMLHTTPRequest(){
+	try{
+		req=new XMLHttpRequest(); /*p.e. firefox*/
+	}catch(err1){
+		try{
+			req=new ActiveXObject("Msxml2.XMLHTTP");/*Algunas versiones de I.E.*/
+		}catch(err2){
+			try{
+				req=new ActiveXObject("Microsoft.XMLHTTP");/*Algunas versiones de I.E.*/
+			}catch(err3){
+				req=false;
+			}
+		}
+	}
+	return req;
+}
+
+var miPeticion=getXMLHTTPRequest();
+
+function solicitudAjaxCliente(){
+	var myurl='textoservidor.php';
+	myRand=parseInt(Math.random()*99999999999);
+	var modurl=myurl+"?rand="+myRand;
+	miPeticion.open("GET", modurl, true);
+	miPeticion.onreadystatechange=manejarRespuestaAjaxServidor;
+	miPeticion.send(null);
+}
+
+function manejarRespuestaAjaxServidor() {
+    if (miPeticion.readyState == 4) {
+        if (miPeticion.status == 200) {
+		    var miTexto=miPeticion.responseText;
+		    $('MiElementoPagina').innerHTML=miTexto;
+        } else {
+			$('MiElementoPagina').innerHTML="";
+    	}
+	}else{//si readyState ha cambiado, pero readySate<>4
+			//[Hacer algo aquí para proveer feedback al usuario]
+	} 
+}
+
---
+++ ajax10/index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mi Aplicación Ajax</title>
+	<script language="JavaScript" type="text/javascript" src="prototype.js "></script>
+	<script language="JavaScript" type="text/javascript" src="ajax_v2.js"></script>
+</head>
+<body onLoad="solicitudAjaxCliente()">
+	<b>El servidor dice: </b><br>
+	<div id="MiElementoPagina"></div>
+</body>
+</html>
