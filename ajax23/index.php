2da3da62553efff019cc063df491a4d23d21d182
Robert Moreira <robertmoreiracenteno@hotmail.com>
2022-03-15 00:30:18 -0500

    "Se está utilizando a prototype.js en los métodos getXMLHTTPRequest y manejarRespuestaAjaxServidor.

---
+++ ajax23/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Probando prototype.js</title>
+	<script language="JavaScript" type="text/javascript" src="prototype.js "></script>
+	<script language="JavaScript" type="text/javascript" src="ajax.js"></script>
+</head>
+<body onLoad="solicitudAjaxCliente()">
+	<b>El servidor dice: </b><br>
+	<div id="MiElementoPagina"></div>
+</body>
+</html>
