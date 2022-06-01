36ce25de915b95ac4f3f393a2b8aac0184adfc00
Robert Moreira <robertmoreiracenteno@hotmail.com>
2022-06-01 13:40:04 -0500

    Crear un array literal en javascript para luego mostrarlo usando un gestor de eventos en la etiqueta body.

---
+++ javascript/index1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Creación de aplicación basada en rico</title>
+	<script>
+		function mostrandoArrayLiteral(){
+			var arreglo=['Andrea',"Lolie", true, null, 30];
+			alert (arreglo[0]);
+			alert (arreglo[1]);
+			alert (arreglo[2]);
+			alert (arreglo[3]);
+			alert (arreglo[4]);
+		}
+		
+	</script>
+</head>
+<body onload="mostrandoArrayLiteral()">
+<body>
+</html>
