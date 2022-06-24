794744c4892d8772e44c9ae98d9a71da446b8aff
Robert Moreira <robertmoreiracenteno@hotmail.com>
2022-06-24 11:52:58 -0500

    Mezclar arrays o objetos literales de javascript para aprender como hacer esta combinación en sintaxis JSON. El objeto literal “oCocheInfo” tiene un array literal dentro, el array literal “aCoches” tiene un objeto literal dentro.

---
+++ javascript/index4.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Javascript, mezclando objetos y arrays literales</title>
+	<script>
+		//Objeto que contiene un array
+		var oCocheInfo={
+			"coloresDisponibles":["rojo","blanco","azul"],
+			"puertasDisponibles":[2,4]
+		};
+
+		//Array que contiene un objeto
+		var aCoches=[
+			{"color":"rojo","puertas":2,"pagado":true},
+			{"color":"azul","puertas":4,"pagado":true},
+			{"color":"blanco","puertas":2,"pagado":false}
+		];
+
+		function sintaxisMostrarArray(){
+			alert(oCocheInfo.coloresDisponibles[1]); //Imprime "blanco"
+			alert(aCoches[1].puertas);//Imprime 4
+		}
+		
+	</script>
+</head>
+<body onload="sintaxisMostrarArray()">
+<body>
+</html>
