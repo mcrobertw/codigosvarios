5791f6a50822f02626ce99111ac38206a920e57f
Robert Moreira <robertmoreiracenteno@hotmail.com>
2022-07-06 12:56:50 -0500

    Mostrar el uso de la función eval para convertir un string que viene del servidor (en este caso ese proceso se abstrae en una línea en la que se lo declara, evitando así complejidad en el ejercicio) con el propósito de almacenarlo en un objeto literal en javascript.

---
+++ javascript/index5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Convirtiendo un string JSon a array literal javascript</title>
+	<script>
+		var sJSON="{\"coloresDisponibles\":[\"rojo\",\"azul\"],\"puertasDisponibles\":[2,4]}"
+		var oCocheInfo=eval("("+sJSON+")");
+
+		function sintaxisMostrarArray(){
+			alert(oCocheInfo.coloresDisponibles[0]); //Imprime "rojo"
+			alert(oCocheInfo.puertasDisponibles[1]);//Imprime 4
+		}
+		
+	</script>
+</head>
+<body onload="sintaxisMostrarArray()">
+<body>
+</html>
