e9297d736d799c654d12e5fc74022f2e4831ebaf
Robert Moreira <robertmoreiracenteno@hotmail.com>
2022-09-17 23:40:21 -0500

    Utilizar el método “createTextRange” de Internet explorer y “setSelectionRange” de Firefox para seleccionar los 3 primeros caracteres de cualquier cadena porque se demuestra la incompatibilidad de ciertos métodos javascript en varios navegadores

---
+++ javascript/index8.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Seleccionando partes de texto</title>
+	<script type="text/javascript">
+		function seleccionCajaTexto(){
+			//alert("hola");
+			var oTextbox=document.getElementById("cajatexto");
+			if(oTextbox.createTextRange)
+			{	var oRange=oTextbox.createTextRange();
+				oRange.moveStart("character",0);
+				oRange.moveEnd("character",3-oTextbox.value.length);
+				oRange.select();
+				document.title="\"createTextRange\" de IE";
+			}
+			else if(oTextbox.setSelectionRange)
+			{	
+				oTextbox.setSelectionRange(0,3);
+				document.title="\"setSelectionRange\" de Firefox";
+			}
+			oTextbox.focus();
+		}
+		var getBrowserInfo = function() {
+		     var ua= navigator.userAgent, tem, 
+		     M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+		    if(/trident/i.test(M[1])){
+		         tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+		        return 'IE '+(tem[1] || '');
+		    }
+		    if(M[1]=== 'Chrome'){
+		        tem= ua.match(/\b(OPR|Edg)\/(\d+)/);
+		        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+		    }
+		    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+		    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+		    return M.join(' ');
+		};
+		function cargarTipoNavegador(){
+			document.getElementById("tiponavegador").innerHTML=getBrowserInfo();
+		}
+	</script>
+</head>
+<body onload="cargarTipoNavegador()">
+	<table border=0>
+		<tr bgcolor=#cccccc>
+			<td colspan=2 align=center id="tiponavegador"><font face="Arial, Helvetica, sans-serif"></font></td>
+		</tr>
+		<tr>
+			<td><font face="Arial, Helvetica, sans-serif">Texto:</font></td>
+			<td align=center><font face="Arial, Helvetica, sans-serif"> 
+					<input type="text" id="cajatexto" size=55 maxlength=50 value="Es bueno dejar la bebida, lo malo es no acordarse d&oacute;nde.">
+			</font></td>
+		</tr>
+		<tr>
+			<td colspan=2 align=center>
+				<button onclick="seleccionCajaTexto()">Seleccionar</button>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
